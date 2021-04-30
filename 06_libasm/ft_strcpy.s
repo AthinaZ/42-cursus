1fa2eb45a3ce82ae06e3ef2babbad84ca26c5e64
Athina Zouzoulas <azouzoul@e4r4p1.42roma.it>
2021-04-30 20:15:47 +0200

    libasm

---
+++ 06_libasm/ft_strcpy.s
@@ -0,0 +1,15 @@
+section .text
+	global	_ft_strcpy
+
+_ft_strcpy:
+	mov			rax, rdi				; char *ret = &dest
+	mov			rcx, -1					; i = -1
+
+_copy:
+	inc			rcx						; i++
+	mov			dh, BYTE[rsi + rcx]		; char temp = src[i]
+	mov			BYTE[rdi + rcx], dh		; dest[i] = temp
+	cmp			BYTE[rsi + rcx], 0
+	jne			_copy					; while (s[i] != 0)
+	ret									; return (ret)
+
---
+++ 06_libasm/ft_strlen.s
@@ -0,0 +1,11 @@
+section .text
+	global _ft_strlen
+
+_ft_strlen:
+	mov			rax, -1				; i = -1
+
+_start:
+	inc			rax					; i++
+	cmp byte	[rdi + rax], 0
+	jne			_start				; while (s[i] != 0)
+	ret								; return (i)
